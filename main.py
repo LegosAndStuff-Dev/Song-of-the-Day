b55ede44f2fe1cc539ba4b78cb6f027a7fab30b8
LegosAndStuff <masonnorberg@midco.net>
2023-12-06 19:21:26 -0600

    started working with spotify api

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 from flask import Flask, render_template
 from flask_restful import Api, Resource
+from dotenv import load_dotenv
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import spotipy.util as util
+import os
+
+load_dotenv()
+
+USERNAME = os.getenv("USERNAME")
+SCOPE = os.getenv("SCOPE")
+CLIENT_ID = os.getenv("CLIENT_ID")
+SECRET = os.getenv("SECRET")
+REDIRECT_URI = os.getenv("REDIRECT_URI")
+
+token = util.prompt_for_user_token(USERNAME,scope=SCOPE,client_id=CLIENT_ID,client_secret=SECRET, redirect_uri=REDIRECT_URI)
+
+if token:
+    sp = spotipy.Spotify(auth=token)
 
 app = Flask(__name__)
 api = Api(app)
 
 class HelloWorld(Resource):
     def get(self, search):
-        return {"Hello World": search}
+        top_artists = sp.current_user_top_artists(limit=5, time_range="medium_term")
+        return top_artists
     
 api.add_resource(HelloWorld, "/hello/<string:search>")
 
