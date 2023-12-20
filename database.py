3241cf208f36614721c2b05b1bb946152f08f8e5
LegosAndStuff <masonnorberg@midco.net>
2023-12-20 07:05:17 -0600

    made makeTable fucntion

--- database.py
+++ database.py
@@ -2,33 +2,34 @@ import sqlite3
 
 conn = sqlite3.connect("static\database\song.sqlite")
 cursor = conn.cursor()
-"""
-cursor.execute("DROP TABLE IF EXISTS SONG")
 
-table = "
-        CREATE TABLE SONG (
-            date TEXT,
-            year INT,
-            month INT,
-            day INT,
-            songName TEXT,
-            trackID TEXT,
-            happy INT,
-            sad INT,
-            Upbeat INT,
-            Calm INT
+def makeTable()
+    cursor.execute("DROP TABLE IF EXISTS SONG")
 
-        );
-"
+    table = """
+            CREATE TABLE SONG (
+                date TEXT,
+                year INT,
+                month INT,
+                day INT,
+                songName TEXT,
+                trackID TEXT,
+                happy INT,
+                sad INT,
+                Upbeat INT,
+                Calm INT
 
-cursor.execute(table)
+            );
+    """
 
-print("Song table is now ready")
+    cursor.execute(table)
 
-conn.commit()
+    print("Song table is now ready")
 
-conn.close()
-"""
+    conn.commit()
+
+    conn.close()
+
 def test():
     cursor.execute(f"""
     INSERT INTO SONG VALUES ("2023-12-20", 20223, 12, 20, "Daylight", "1odExI7RdWc4BT515LTAwj", 1, 0, 1, 0)
